8a2fd8169efd6af50836b191e261e8e7549bc17c
Majulia <majulialcarvalho@gmail.com>
2024-06-03 11:57:54 -0300

    creating tables

--- Database/refreshingInformations.sql
+++ Database/refreshingInformations.sql
@@ -5,5 +5,9 @@ DROP DATABASE[Curso]
 USE [Curso]
 /*schema como dados são definidos e lidos no bd*/
 CREATE TABLE [Aluno](
-    
+    [Id] INT,
+    [Nome] NVARCHAR(80),
+    [Nascimento] DATETIME,
+    [Active] BIT 
 )
+GO
