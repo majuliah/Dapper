f1a42238366381cbe8c59dcc6b87f5ae545fbfb0
Majulia <majulialcarvalho@gmail.com>
2024-06-03 14:28:20 -0300

    constraints not null/default

--- Database/refreshingInformations.sql
+++ Database/refreshingInformations.sql
@@ -24,3 +24,23 @@ ALTER TABLE [Aluno]
     DROP COLUMN [Document] 
 GO
 
+DROP TABLE [Aluno]
+
+
+/* constraints */
+
+CREATE TABLE [Aluno](
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [Nascimento] DATETIME NULL,
+    [Active] BIT DEFAULT(0)
+)
+GO
+
+ALTER TABLE [Aluno]
+    ALTER COLUMN [Active] BIT NOT NULL
+GO
+
+ALTER TABLE [Aluno]
+    ALTER COLUMN [Active] BIT NOT NULL
+GO
